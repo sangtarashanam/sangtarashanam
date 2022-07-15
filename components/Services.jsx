00f61c89383861356c34b02aecde4040fdf868d0
sangtarashanam <sangtarashanam@gmail.com>
2022-07-15 12:06:58 +0430

    add services component

---
+++ components/Services.jsx
@@ -0,0 +1,25 @@
+function Services (){
+    return(
+         <>
+         <div>
+         <div> <span className=" text-xs p-6"> ------------------    آنچه من انجام میدهم</span></div>
+         <div>
+             <h2 className="  text-3xl p-6"> خدمات من</h2>
+         </div>
+
+         </div>
+         <div className=" p-16 grid grid-cols-2 lg:grid-cols-3">
+         <div className=" flex p-2"><img className=" w-12 h-8 text-2xl" src={"./5.svg"}/> <span> طراحی وب سایت</span></div>
+         <div className=" flex p-2"><img className=" w-12 h-8 text-2xl" src={"./6.svg"}/> <span>بهینه سازی وب سایت</span></div>
+         <div className=" flex p-2"><img className=" w-12 h-8 text-2xl" src={"./7.svg"}/> <span>بهبود تجربه کاربری سایت</span></div>
+         <div className=" flex p-2"><img className=" w-12 h-8 text-2xl" src={"./8.svg"}/> <span>طراحی قالب وردپرس</span></div>
+         <div className=" flex p-2"><img className=" w-12 h-8 text-2xl" src={"./9.svg"}/> <span>دیجیتال مارکتینگ</span></div>
+         <div className=" flex p-2"><img className=" w-12 h-8 text-2xl" src={"./13.svg"}/> <span>مشاوره کسب وکار</span></div>
+
+         </div>
+         
+         </>
+     )
+     
+}
+export default Services;
