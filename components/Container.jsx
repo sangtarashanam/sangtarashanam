820491f1950983acd5b32f7f6315fde47e0af973
sangtarashanam <sangtarashanam@gmail.com>
2022-07-26 13:31:56 +0430

    add CV

--- components/Container.jsx
+++ components/Container.jsx
@@ -1,66 +1,64 @@
 import Button from "./Button";
-import Services from '../components/Services'
+import Services from "../components/Services";
+import WorkSamples from "../components/WorkSamples";
+import Cv from "./cv";
 
-function Container (){
-    return(
-        <>
-        
-
-        <div id="main" className="flex flex-col  justify-center items-center">
-            <div className=" 	  bg-my_bg_image  flex items-center justify-center h-screen w-full ">
-               <div className=" flex flex-col justify-center items-center p-4">
-
-            <img className=" h-52 w-52 rounded-full m-5" src={"./2.jpg"}/>
+function Container() {
+  return (
+    <>
+      <div id="main" className="flex flex-col  justify-center items-center">
+        <div className=" 	  bg-my_bg_image  flex items-center justify-center h-screen w-full ">
+          <div className=" flex flex-col justify-center items-center p-4">
+            <img className=" h-52 w-52 rounded-full m-5" src={"./2.jpg"} />
             <p className=" text-3xl p-6">محمد مهدی سنگتراشان</p>
-            <p className=" text-1xl p-7">محمد مهدی سنگتراشان هستم طراح سایت و علاقه مند به فرانتند 
-                اگر مایل به داشتن سایتی به روز و باکیفیت هستید در خدمت هستم
+            <p className=" text-1xl p-7">
+              محمد مهدی سنگتراشان هستم طراح سایت و علاقه مند به فرانتند اگر مایل
+              به داشتن سایتی به روز و باکیفیت هستید در خدمت هستم
             </p>
-            <div  className=" p-8">
+            <div className=" flex flex-col sm:flex-row " >
+              <a  href="/#contact-us">
 
-            <Button className="mr-2 w-32" color="blue">
+              <Button className="mr-2 w-32" color="blue">
                 تماس با من
               </Button>
+              </a>
+              <a  target="blank" href="https://www.instagram.com/mahdi_sangtarashan/">
+
               <Button  className="mr-2 w-32" color="blue">
                 پیج کاری من
               </Button>
-              
+              </a>
             </div>
-
-                
-               </div>
-                   </div>
-            <div id="Services" className="  p-2 h-screen w-full  ">
-                
-
-               <Services/>
-                    </div>
-            <div id="Work-samples" className=" flex items-center justify-center h-screen w-full ">
-                
-
-                <samp> نمونه کار
-                    </samp>
-                   </div>
-            <div id="Cv" className=" flex items-center justify-center h-screen w-full bg-sky-400">
-                
-
-                <samp>رزومه
-                    </samp>
-                    </div>
-            <div id="blog" className=" flex items-center justify-center h-screen w-full bg-sky-500">
-                
-
-                <samp>
-                بلاگ
-                    </samp>
-                    </div>
-            <div id="contact-us" className=" flex items-center justify-center h-screen w-full bg-sky-600">
-                
-
-                <samp> تماس باما
-                    </samp>
-                   </div>
+          </div>
         </div>
-        </>
-    )
+        <div id="Services" className=" h-screen w-full flex-1 mt-24 ">
+          <Services />
+        </div>
+        <div id="Work-samples" className="h-screen  w-full flex-1 mt-24">
+        
+          <WorkSamples />
+        </div>
+        <div
+          id="Cv"
+          className="flex items-center justify-center mt-24"
+        >
+          <div></div>
+          <Cv/>
+        </div>
+        <div
+          id="blog"
+          className=" flex items-center justify-center h-screen w-full bg-sky-500"
+        >
+          <samp>بلاگ</samp>
+        </div>
+        <div
+          id="contact-us"
+          className=" flex items-center justify-center h-screen w-full bg-sky-600"
+        >
+          <samp> تماس باما</samp>
+        </div>
+      </div>
+    </>
+  );
 }
 export default Container;
---
+++ components/cv.jsx
@@ -0,0 +1,136 @@
+const Cv = () => {
+  return (
+    <>
+    <div className=" m-2 mt-20 ">
+
+      <div>
+        <span>------------------ نگاهی بیندازید به</span>
+        <h2 className=" p-8 text-2xl font-bold">
+          رزومه کاری من در ده سال اخیر{" "}
+        </h2>
+      </div>
+      <div class="md:w-2/3 lg:mr-10 ">
+        <div class="relative mt-5 text-right">
+          <div class="flex items-center relative">
+            <div class="hidden md:block w-20">
+              <div class="font-bold italic">۱۳۸۹-۱۳۹۲</div>
+              <div class="md:flex space-x-1 text-xs">
+                <div>خرداد.</div>
+                <div>-</div>
+                <div>آبان.</div>
+              </div>
+            </div>
+
+            <div class="border-r-2 border-black absolute h-full right-1 md:right-20 top-2 z-10">
+              <i class="fas fa-circle -top-1 -mr-2 absolute"></i>
+            </div>
+
+            <div class="mr-10">
+              <div class="font-bold">سردبیر بخش گیشه</div>
+              <div class="italic md:mb-4">جام نیوز</div>
+              <div class="mb-4 mt-2 md:hidden">
+                <div class="font-bold">1389-1392</div>
+                <div class="text-xs">آبان - خرداد</div>
+              </div>
+              <div class="mb-10">
+                {" "}
+                به مدت سه سال در بخشهای مختلف جام نیوز فعالیت داشتم و سال اخر به
+                عوان سردبیر بخش گیشه جام نیوز فعالیت داشتم که لوح تقدیر برترین
+                قسمت تحریریه را دریافت کردم
+              </div>
+            </div>
+          </div>
+
+          <div class="flex items-center relative">
+            <div class="hidden md:block w-20">
+              <div class="font-bold italic">۱۳۹۲-۱۳۹۳</div>
+              <div class="md:flex space-x-1 text-xs">
+                <div>دی.</div>
+                <div>-</div>
+                <div>اسفند.</div>
+              </div>
+            </div>
+
+            <div class="border-r-2 border-black absolute h-full right-1 md:right-20 top-2 z-10">
+              <i class="fas fa-circle -top-1 -mr-2 absolute"></i>
+            </div>
+
+            <div class="mr-10">
+              <div class="font-bold">سامانه های واتساپ</div>
+              <div class="italic md:mb-4">شرکت خودم</div>
+              <div class="mb-4 mt-2 md:hidden">
+                <div class="font-bold">۱۳۹۲-۱۳۹۵</div>
+                <div class="text-xs">اسفند - دی</div>
+              </div>
+              <div class="mb-10">
+                {" "}
+                ایده پردازی و راه اندازی سامانه های واتساپ شبیه به ربات های فعلی
+                تلگرام و همچنین ارسال تبلیعات انبوه در واتساپ و تلگرام وساخت
+                شبکه بازاریابی قوی در این حوزه
+              </div>
+            </div>
+          </div>
+          <div class="flex items-center relative">
+            <div class="hidden md:block w-20">
+              <div class="font-bold italic">۱۳۹۴-۱۳۹۵</div>
+              <div class="md:flex space-x-1 text-xs">
+                <div>بهمن.</div>
+                <div>-</div>
+                <div>اسفند.</div>
+              </div>
+            </div>
+
+            <div class="border-r-2 border-black absolute h-full right-1 md:right-20 top-2 z-10">
+              <i class="fas fa-circle -top-1 -mr-2 absolute"></i>
+            </div>
+
+            <div class="mr-10">
+              <div class="font-bold">رئیس دفتر </div>
+              <div class="italic md:mb-4"> کانون مساجد کل کشور</div>
+              <div class="mb-4 mt-2 md:hidden">
+                <div class="font-bold">۱۳۹۴-۱۳۹۵</div>
+                <div class="text-xs">اسفند - دی</div>
+              </div>
+              <div class="mb-10">
+                {" "}
+                ریس دفتر مدیر کل کانون مساجد کشور بودم همزمان به تحصیل ادامه میدادم
+              </div>
+            </div>
+          </div>
+
+          <div class="flex items-center relative">
+            <div class="hidden md:block w-20">
+              <div class="font-bold italic">۱۳۹۵-۱۴۰۰</div>
+              <div class="md:flex space-x-1 text-xs">
+                <div>فروردین.</div>
+                <div>-</div>
+                <div>اسفند.</div>
+              </div>
+            </div>
+
+            <div class="border-r-2 border-black absolute h-full right-1 md:right-20 top-2 z-10">
+              <i class="fas fa-circle -top-1 -mr-2 absolute"></i>
+              <i class="fas fa-circle bottom-0 -mr-2 absolute"></i>
+            </div>
+
+            <div class="mr-10">
+              <div class="font-bold">مدیر و ایده پرداز</div>
+              <div class="italic md:mb-4">رادیکال</div>
+              <div class="mb-4 mt-2 md:hidden">
+                <div class="font-bold">۱۳۹۵ - ۱۴۰۰</div>
+                <div class="text-xs">فروردین - اسفند</div>
+              </div>
+              <div class="mb-10">
+                ایده پرداز و دیجیتال مارکت تیم هم بودم و در کل در این چند سال
+                چندین اپ پرفروش و با تعداد نصب بالا همچون فلگرام با تعداد نصب
+                بیش از ششصد هزار تا از جمله فعالیت های موفق من در این سالها بود
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+};
+export default Cv;
